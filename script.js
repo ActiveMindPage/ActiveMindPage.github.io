39433753a50367808f5d41eca31352a958691f77
ActiveMindPage <access@activemind.page>
2023-05-21 22:40:39 +0800

    add js

---
+++ script.js
@@ -0,0 +1,15 @@
+window.onload = function () {
+  var image = document.getElementById("centered-image");
+
+  function animateImage() {
+    image.style.transform = "scale(1.5)";
+    image.style.opacity = "0.5";
+    setTimeout(function () {
+      image.style.transform = "scale(1)";
+      image.style.opacity = "1";
+    }, 2000);
+    setTimeout(animateImage, 4000);
+  }
+
+  animateImage();
+};
