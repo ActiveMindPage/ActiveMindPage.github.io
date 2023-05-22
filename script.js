720aa92efda58dbb08163b703997e76d51a4dc16
ActiveMindPage <access@activemind.page>
2023-05-22 17:14:28 +0800

    Changed design and added buttons

--- script.js
+++ script.js
@@ -1,16 +1,11 @@
 window.onload = function () {
   var image = document.getElementById("centered-image");
-  image.classList.add("animate");
+  var buttons = document.querySelectorAll(".button");
 
-  function animateImage() {
-    image.style.transform = "scale(1.5)";
-    image.style.opacity = "0.5";
-    setTimeout(function () {
-      image.style.transform = "scale(1)";
-      image.style.opacity = "1";
-    }, 2000);
-    setTimeout(animateImage, 4000);
-  }
-
-  animateImage();
+  setTimeout(function () {
+    image.style.opacity = "0";
+    buttons.forEach(function (button) {
+      button.classList.add("fade-in");
+    });
+  }, 2000);
 };
