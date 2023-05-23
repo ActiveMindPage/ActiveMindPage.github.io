575b50a3b4a7890fa059254d51fb1239098c9b71
ActiveMindPage <134210967+ActiveMindPage@users.noreply.github.com>
2023-05-23 09:05:28 +0800

    Adding link & Text

--- script.js
+++ script.js
@@ -3,9 +3,21 @@ window.onload = function () {
   var buttons = document.querySelectorAll(".button");
 
   setTimeout(function () {
-    image.style.opacity = "0";
+    image.style.opacity = "0"; // Disappears the image
+    image.style.pointerEvents = "none"; // Removes image entirely
     buttons.forEach(function (button) {
       button.classList.add("fade-in");
     });
-  }, 2000);
+  }, 1000);
 };
+
+function handleButtonClick(buttonNumber) {
+  console.log("Button " + buttonNumber + " clicked!");
+}
+
+window.addEventListener("load", function () {
+  var image = document.getElementById("centered-image");
+  image.addEventListener("animationend", function () {
+    image.style.display = "none";
+  });
+});
